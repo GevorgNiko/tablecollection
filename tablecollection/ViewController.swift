8d4835ecdeb911b7868566e66ea1825c70ed3cd2
Gevorg <gevorggsh@gmail.com>
2020-06-08 16:39:01 +0400

    Initial Commit

---
+++ tablecollection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tablecollection
+//
+//  Created by Nikoghosyan Gevorg on 6/8/20.
+//  Copyright © 2020 Nikoghosyan Gevorg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
